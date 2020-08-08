51494bcb3cba73861ac1f5e0b83b8b2ef1978b73
Tobias Palmer <tobiaspalmer93@gmail.com>
2020-08-08 18:17:57 +0200

    tests, docker, dpow

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 // config.js
 module.exports = {
   nodeRpc: process.env.NODE_RPC || 'http://localhost:7076',
-  authkey: process.env.AUTH_KEY || null,
+  authKey: process.env.AUTH_KEY || null,
   points: process.env.POINTS || 500,
   duration: process.env.DURATION || 60 * 60,
-  pointswork: process.env.POINTS_WORK || 50,
+  pointsForWork: process.env.POINTS_WORK || 50,
+  allowWork: process.env.ALLOW_WORK || true,
+  dpowUser: process.env.DPOW_USER || undefined,
+  dpowKey: process.env.DPOW_KEY || undefined
 }
